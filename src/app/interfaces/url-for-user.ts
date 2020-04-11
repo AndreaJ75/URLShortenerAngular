f9185dcb28c3c4fd902694cc63d3c3f67c10fb96
AndyJ75 <andrea.jouannin.devweb@gmail.com>
2020-04-11 13:20:13 +0200

    add edit & update & delete on urlLinks

--- src/app/interfaces/url-for-user.ts
+++ src/app/interfaces/url-for-user.ts
@@ -1,6 +1,7 @@
 import DateTimeFormat = Intl.DateTimeFormat;
 
 export interface UrlForUser {
+  id : number;
   urlLong: string;
   maxClickNumber: number;
   expirationDate: DateTimeFormat;
--- src/app/services/url-creation.service.ts
+++ src/app/services/url-creation.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { UrlLink } from '../interfaces/url-link';
-import {API_URL} from '../app.constants';
+import {API_URL, USER_URL} from '../app.constants';
 import {UrlForUser} from '../interfaces/url-for-user';
 
 
@@ -20,14 +20,32 @@ export class UrlCreationService {
     return this.http.post<UrlLink>(urlForCreation, urlLong);
   }
 
+  getUrlLinkById(urlLinkId): Observable<UrlLink> {
+    console.log('********** get UrllinkById');
+    const urlForGetById = API_URL + USER_URL + '/getById/' + urlLinkId;
+    console.log('URL for GETBy ID = ' + urlForGetById);
+    return this.http.get<UrlLink>(urlForGetById);
+  }
+
   getUrlLinks(): Observable<any> {
-    const urlForCreation = API_URL + 'urlLinks/user/getsorted';
-    return this.http.get<any>(urlForCreation);
+    const urlForGetUsersUrls = API_URL + USER_URL + '/getsorted';
+    return this.http.get<any>(urlForGetUsersUrls);
   }
 
   createUrlLinkForUser(urlLongForUser: UrlForUser):Observable<UrlLink> {
-
-    const urlForCreation = API_URL + 'urlLinks/user';
+    const urlForCreation = API_URL + USER_URL;
     return this.http.post<UrlLink>(urlForCreation, urlLongForUser);
+  }
+
+  updateUrlFeedLinkForUser(urlLongForUser:UrlForUser): Observable<UrlLink> {
+    const urlForUpdate = API_URL + USER_URL;
+    return this.http.put<UrlLink>(urlForUpdate, urlLongForUser);
+  }
+
+  delUrllLink(urlLinkIdToDelete:number) : Observable<any>{
+    const urlForDelete = API_URL + USER_URL +'/deleteurl/'+ urlLinkIdToDelete ;
+    console.log('urlfor delete = ' + urlForDelete);
+    //return this.http.delete<any>(urlForDelete +`${urlLinkIdToDelete}`);
+    return this.http.delete<any>(urlForDelete);
   }
 }
